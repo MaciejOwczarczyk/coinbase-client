78818e0e8ac04b256a053debc9a0b73dae17e07e
Maciej Owczarczyk <maciej.owczarczyk@gmail.com>
2021-11-30 11:31:46 +0100

    first commit

---
+++ src/main/java/pl/owczarczyk/coinbase/CoinbaseClientApplication.java
@@ -0,0 +1,13 @@
+package pl.owczarczyk.coinbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinbaseClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinbaseClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/owczarczyk/coinbase/CoinbaseClientApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.owczarczyk.coinbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinbaseClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
